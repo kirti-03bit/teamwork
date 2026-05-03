9057972dc9f58235d0322ad0fa0c5bae673d351a
Kirti <nalbalekirti@gmail.com>
2026-05-03 22:17:39 +0530

    first commit

---
+++ backend/src/controllers/member.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { z } from "zod";
+import { HTTPSTATUS } from "../config/http.config";
+
+import { asyncHandler } from "../middlewares/asyncHandker.middleware";
+import { joinWorkspaceByInviteService } from "../services/member.auth";
+
+export const joinWorkspaceController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const inviteCode = z.string().parse(req.params.inviteCode);
+    const userId = req.user?._id;
+
+    const { workspaceId, role } = await joinWorkspaceByInviteService(
+      userId,
+      inviteCode
+    );
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Successfully joined the workspace",
+      workspaceId,
+      role,
+    });
+  }
+);
---
+++ backend/src/index.ts
@@ -0,0 +1,78 @@
+import session from "cookie-session";
+import cors from "cors";
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import passport from "passport";
+import { config } from "./config/app.config";
+import connectDatabase from "./config/database.config";
+import { HTTPSTATUS } from "./config/http.config";
+
+import "./config/passport.config";
+import { ErrorCodeEnum } from "./enums/error-code.enum";
+import { asyncHandler } from "./middlewares/asyncHandker.middleware";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import isAuthenticated from "./middlewares/isAuthenticated.middleware";
+
+import authRoutes from "./routes/authroute";
+import memberRoutes from "./routes/member.route";
+import projectRoutes from "./routes/project.route";
+import taskRoutes from "./routes/task.route";
+import userRoutes from "./routes/user.route";
+import workspaceRoutes from "./routes/workspace.route";
+import { BadRequestException } from "./utils/appError";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+app.get(`/`,
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+     throw new BadRequestException(
+      "This is a bad request",
+      ErrorCodeEnum.AUTH_INVALID_TOKEN
+    );
+      
+     return res.status(HTTPSTATUS.OK).json({
+      message: "Hello Subscribe to the channel & share",
+    });
+  })
+);
+app.use(`${BASE_PATH}/auth`, authRoutes);
+app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
+app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
+app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
+app.use(`${BASE_PATH}/task`, isAuthenticated, taskRoutes);
+
+
+
+
+app.use(errorHandler);
+
+
+  app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+  await connectDatabase()
+});
